c07db0abe193b318ab83bbe67df38dc4edf10806
Panos Anastasiadis <anastapd@gmail.com>
2021-04-25 11:50:14 +0100

    Integrated react router

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // Startup point for the client side application
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import Routes from "./Routes";
+import { BrowserRouter, Route } from "react-router-dom";
 
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>,
+    document.querySelector("#root")
+);
